24aaa43baaaa47a745014bb0005a96d2b5a56aa2
Garrett Luu <garrettluu131@gmail.com>
2020-03-23 19:46:47 -0700

    ported to next.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+module.exports = function (app) {
+  var express = require('express');
+  var router = express.Router();
+  let handle = app.getRequestHandler();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  /* GET home page. */
+  router.get('*', function (req, res, next) {
+    // res.render('index', { title: 'Express' });
+    return handle(req, res);
+  });
 
-module.exports = router;
+  return router;
+};
+

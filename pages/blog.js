f58bd81e35571243f14d0b707bef4d5a194a224a
Garrett Luu <garrettluu131@gmail.com>
2020-03-29 00:39:22 -0700

    Added blog page

---
+++ pages/blog.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+import BlogCard from "../components/BlogCard";
+
+export default () => {
+    return (
+        <Layout>
+            <div className="header">
+                <p className="header-text">
+                    Blog
+                    <div className="fancy-rectangle"></div>
+                </p>
+            </div>
+            <div>
+                <BlogCard title="Why Am I Studying CS?"
+                          date="2020.2.19">
+                    <p className="body-text">
+                        Lorem ipsum
+                    </p>
+                </BlogCard>
+                <BlogCard title="Why Am I Studying CS?"
+                          date="2020.2.19">
+                    <p className="body-text">
+                        Lorem ipsum
+                    </p>
+                </BlogCard>
+            </div>
+            <style jsx>{`
+                .header {
+                    display: flex;
+                    margin: auto;
+                    width: 100%;
+                    height: 512px;
+
+
+                    background: url("/images/titlebg.jpg") no-repeat;
+                    background-size: cover;
+                    background-position: center;
+                }
+
+                .header-text {
+                    margin: auto;
+                    text-align: center;
+
+                    font-family: 'Oswald';
+                    font-size: 72px;
+                    color: white;
+                }
+            `}
+            </style>
+        </Layout>
+    );
+};
